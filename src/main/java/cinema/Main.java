c5dccda01c66cb08420662614676703d49856184
Yura Lug <LUV97@ukr.net>
2021-08-11 16:53:25 +0300

    Created cinema-shop app, using hibernate.

---
+++ src/main/java/cinema/Main.java
@@ -0,0 +1,66 @@
+package cinema;
+
+import cinema.lib.Injector;
+import cinema.model.CinemaHall;
+import cinema.model.Movie;
+import cinema.model.MovieSession;
+import cinema.model.ShoppingCart;
+import cinema.model.User;
+import cinema.security.AuthenticationService;
+import cinema.service.CinemaHallService;
+import cinema.service.MovieService;
+import cinema.service.MovieSessionService;
+import cinema.service.OrderService;
+import cinema.service.ShoppingCartService;
+import java.time.LocalDateTime;
+
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("cinema");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+    private static final CinemaHallService cinemaHallService =
+            (CinemaHallService) injector.getInstance(CinemaHallService.class);
+    private static final MovieSessionService movieSessionService =
+            (MovieSessionService) injector.getInstance(MovieSessionService.class);
+    private static final AuthenticationService authenticationService =
+            (AuthenticationService) injector.getInstance(AuthenticationService.class);
+    private static final ShoppingCartService shoppingCartService =
+            (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+    private static final OrderService orderService =
+            (OrderService) injector.getInstance(OrderService.class);
+
+    public static void main(String[] args) {
+        Movie fastAndFurious = new Movie("Fast and Furious");
+        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
+        movieService.add(fastAndFurious);
+        System.out.println(movieService.get(fastAndFurious.getId()));
+        movieService.getAll().forEach(System.out::println);
+        CinemaHall firstCinemaHall = new CinemaHall();
+        firstCinemaHall.setCapacity(100);
+        firstCinemaHall.setDescription("first hall with capacity 100");
+        CinemaHall secondCinemaHall = new CinemaHall();
+        secondCinemaHall.setCapacity(200);
+        secondCinemaHall.setDescription("second hall with capacity 200");
+        cinemaHallService.add(firstCinemaHall);
+        cinemaHallService.add(secondCinemaHall);
+        System.out.println(cinemaHallService.getAll());
+        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
+        MovieSession tomorrowMovieSession = new MovieSession();
+        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
+        tomorrowMovieSession.setMovie(fastAndFurious);
+        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
+        MovieSession yesterdayMovieSession = new MovieSession();
+        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
+        yesterdayMovieSession.setMovie(fastAndFurious);
+        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
+        movieSessionService.add(tomorrowMovieSession);
+        movieSessionService.add(yesterdayMovieSession);
+        User yurka = null;
+        yurka = authenticationService.register("alice", "1234");
+        System.out.println("Can't register user!");
+        ShoppingCart yurkaShoppingCart = shoppingCartService.getByUser(yurka);
+        orderService.completeOrder(yurkaShoppingCart);
+        orderService.getOrdersHistory(yurka).forEach(System.out::println);
+    }
+}
---
+++ src/main/java/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,69 @@
+package cinema.dao.impl;
+
+import cinema.dao.ShoppingCartDao;
+import cinema.exception.DataProcessingException;
+import cinema.lib.Dao;
+import cinema.model.ShoppingCart;
+import cinema.model.User;
+import cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Cannot create Shopping cart ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM ShoppingCart sh "
+                    + "LEFT JOIN FETCH sh.tickets t "
+                    + " WHERE sh.user = :user", ShoppingCart.class).setParameter("user", user)
+                    .uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Cannot find shopping cart using user ", e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Cannot create shopping cart ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
